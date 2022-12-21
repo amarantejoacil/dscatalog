0426b82818379fc74e65d9830badf3f52d96aca4
Joacil Amarante <joacil.amarante@gmail.com>
2022-12-21 09:34:25 -0400

    tratamento de excessão

--- src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,15 +16,11 @@ import com.devsuperior.dscatalog.services.CategoryService;
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
-	/*
-	 * 
-	 * Resource = recursos São os controllers
+	/* Resource = recursos São os controllers
 	 * @RestController = indica que essa classe é uma controladora
 	 * Transforma a classe em um recurso REST...
 	 * 
-	 * 
 	 * @RequestMapping: mapeia a sua rota
-	 * 
 	 * endpoint = são as rotas dentro do recurso
 	 * 
 	 * Responsavél pela interação camara de serviço 	 * 
@@ -37,7 +34,12 @@ public class CategoryResource {
 	public ResponseEntity<List <CategoryDTO>> findAll(){
 		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
-	}
+	}	
 	
-
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+		CategoryDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}	
 }
--- src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -17,13 +19,9 @@ public class CategoryService {
 	/*Responsável por realizar o serviço solicitado pelo usuário
 	 * A lógica de negocio do sistema
 	 * Calcular, Buscar determina usuário
-	 * 
-	 * 
 	 * @Transactional garante transação com banco de dados, garante que será executado na camada de serviço
-	 * 
-	 * readOnly = true não da locking no banco de dados, garantindo mais performance
-	 * 
-	 * */
+	 *  readOnly = true não da locking no banco de dados, garantindo mais performance
+	 *  */
 
 	@Autowired
 	private CategoryRepository repository;
@@ -37,5 +35,16 @@ public class CategoryService {
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 		
 	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found - executando excessão"));
+		return new CategoryDTO(entity);
+	}
+	
+	
+	
+	
 	
 }
